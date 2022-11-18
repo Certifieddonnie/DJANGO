286781deb4d81116361623a8eb49cd3eb8dc9608
Certifieddonnie <mcdonaldsamure91@gmail.com>
2022-11-18 01:08:20 +0100

    MVT

--- certified/polls/urls.py
+++ certified/polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('index/', views.index, name='main-view'),
+    path('add', views.add, name="add")
 ]
--- certified/polls/views.py
+++ certified/polls/views.py
@@ -10,3 +10,11 @@ def index(request):
 
 def home(request):
     return render(request, 'home.html', {'name': 'McDonald', 'age': '18'})
+
+
+def add(request):
+
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 + val2
+    return render(request, 'result.html', {'result': res})
