9908bc4f28db68d785829a2cb96c53e23d6924f5
Certifieddonnie <mcdonaldsamure91@gmail.com>
2022-11-20 03:42:13 +0100

    Progressing

--- certified/polls/urls.py
+++ certified/polls/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('index/', views.index, name='main-view'),
-    path('add', views.add, name="add")
+    # path('indes', views.blah, name="blah"),
+    path('indes', views.index, name='index'),
+    path('home', views.home, name="home"),
+    path('home2', views.home2, name="home2"),
+    path('checker', views.checker, name="checker"),
+    path('check', views.check, name="check"),
+    path('add', views.add, name="add"),
+    path('sub', views.sub, name="sub")
 ]
--- certified/polls/views.py
+++ certified/polls/views.py
@@ -4,12 +4,24 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+def blah(request):
+    return HttpResponse("Hello Blah!")
+
+
 def index(request):
-    return HttpResponse("Hello, World! This is the polls index.")
+    return render(request, 'index.html')
 
 
 def home(request):
     return render(request, 'home.html', {'name': 'McDonald', 'age': '18'})
+
+
+def home2(request):
+    return render(request, 'home2.html', {'name': 'McDonald', 'age': '18'})
+
+
+def checker(request):
+    return render(request, 'checker.html')
 
 
 def add(request):
@@ -18,3 +30,24 @@ def add(request):
     val2 = int(request.POST['num2'])
     res = val1 + val2
     return render(request, 'result.html', {'result': res})
+
+
+def sub(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 - val2
+    return render(request, 'result.html', {'result': res})
+
+# To determine operation to work on.
+
+
+def check(request):
+    oper = request.POST["ops"]
+
+    oper = oper.lower()
+    if oper == "add":
+        return home(request)
+    elif oper == "sub":
+        return home2(request)
+    else:
+        return render(request, 'error.html')
